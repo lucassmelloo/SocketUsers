ab87752c19f03214c5264500af1418ca734d42e7
Lucas Mello <lucasmellovieira99@gmail.com>
2022-10-26 16:55:42 -0300

    Inicializando projeto e listando usuarios

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,57 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+    <Head title="Home" />
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                User List
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <table class="min-w-full leading normal ">
+                            <thead>
+                                <tr class="text-left border-b-2">
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Created at</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="" v-for="user in userList" :key="user.id">
+                                    <td>{{user.id}}</td>
+                                    <td>{{user.name}}</td>
+                                    <td>{{user.email}}</td>
+                                    <td>{{user.created_at}}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
+
+<script>
+export default {
+    props:{
+        userList:[]
+    },
+
+    data(){
+
+    }
+}
+</script>
+<style lang="">
+    
+</style>
