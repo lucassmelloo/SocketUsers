dfc40c303ce5f4d32976e58fea281420de192ff7
Mello <lucasmellovieira99@gmail.com>
2022-10-26 22:21:05 -0300

    Componentizando tabela

---
+++ resources/js/Components/UsersTable.vue
@@ -0,0 +1,42 @@
+<template lang="">
+    <div>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <table class="min-w-full leading normal ">
+                            <thead>
+                                <tr class="text-left">
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Created at</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="text-left border-b-2" v-for="user in userList" :key="user.id">
+                                    <td class="p-1.5">{{user.id}}</td>
+                                    <td><div class="font-semibold text-left">{{user.name}}</div></td>
+                                    <td>{{user.email}}</td>
+                                    <td>{{user.created_at}}</td>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        userList: []
+    }
+}
+</script>
+<style lang="">
+    
+</style>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import UsersTableVue from '@/Components/UsersTable.vue';
 import { Head } from '@inertiajs/inertia-vue3';
 </script>
 
@@ -11,33 +12,7 @@ import { Head } from '@inertiajs/inertia-vue3';
                 User List
             </h2>
         </template>
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
-                        <table class="min-w-full leading normal ">
-                            <thead>
-                                <tr class="text-left border-b-2">
-                                    <th>ID</th>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                    <th>Created at</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr class="" v-for="user in userList" :key="user.id">
-                                    <td>{{user.id}}</td>
-                                    <td>{{user.name}}</td>
-                                    <td>{{user.email}}</td>
-                                    <td>{{user.created_at}}</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <UsersTableVue :userList="userList"></UsersTableVue>
     </AuthenticatedLayout>
 </template>
 
